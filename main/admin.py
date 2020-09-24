d3fe9f80d7ba20a6277759293573cc903e9d1a98
Duupec <71795400+Duupec@users.noreply.github.com>
2020-09-23 19:07:26 -0500

    first commit

---
+++ main/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+
+# Regi# Register your models here.
+
+admin.site.register(Localizacion)
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Proveedor)
